51a8a086dd6d1368486c79f781c810e5aadd12d5
Aakash Vishwakarma <97448935+AakaShVSh@users.noreply.github.com>
2026-05-11 09:42:31 +0530

    Update ResetPassword.jsx

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import API from "../api/apiBase";
-import authStyles from "../styles/authStyles";
+import authStyles from "../styles/AuthStyles";
 
 const EyeIcon = ({ open }) =>
   open ? (
